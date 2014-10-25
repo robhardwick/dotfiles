102bc33434ef8d98153f56856ec901ccbb21d4d3
Rob Hardwick <robhardwick@gmail.com>
2014-10-25 16:02:33 +0100

    Add lxmusic, remove xmms2tray

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -22,11 +22,12 @@ main = do
             , ppTitle = xmobarColor "blue" "" . shorten 50
             , ppLayout = const ""
             }
-        , workspaces = ["1:term", "2:web", "3:video", "4", "5", "6"]
+        , workspaces = ["1:term", "2:web", "3:music", "4:video", "5", "6"]
         , startupHook = do
             spawnOn "1:term" "urxvt -title tmux -e ~/bin/tmux-init.sh"
             spawnOn "2:web" "firefox"
-            spawnOn "3:video" "vlc"
+            spawnOn "3:music" "lxmusic"
+            spawnOn "4:video" "vlc"
         , borderWidth = 0
         , terminal = "urxvt"
         }
