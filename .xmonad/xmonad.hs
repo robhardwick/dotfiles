c36106fd54f7cee3ac8d05b8c7c9738b3512f484
Rob Hardwick <robhardwick@gmail.com>
2014-10-25 15:38:29 +0100

    Configure xmobar and add trayer, nm-applet and xmms2

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,32 @@
 import XMonad
 import XMonad.Actions.SpawnOn
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+import System.IO
 
-main = do xmonad defaultConfig {
-	borderWidth = 0,
-	terminal = "urxvt",
-	workspaces = ["1:term", "2:web", "3:music", "4:video", "5", "6"],
-	startupHook = do
-		spawnOn "1:term" "urxvt -e ~/bin/tmux-init.sh"
-		spawnOn "2:web" "chromium"
-		spawnOn "3:music" "clementine"
-		spawnOn "4:video" "vlc",
-	manageHook = manageSpawn <+> manageHook defaultConfig
-}
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.config"
+    xmonad $ defaultConfig
+        { manageHook = composeAll
+            [ manageDocks
+            , manageSpawn
+            , isFullscreen --> doFullFloat
+            , manageHook defaultConfig
+            ]
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "blue" "" . shorten 50
+            , ppLayout = const ""
+            }
+        , workspaces = ["1:term", "2:web", "3:video", "4", "5", "6"]
+        , startupHook = do
+            spawnOn "1:term" "urxvt -title tmux -e ~/bin/tmux-init.sh"
+            spawnOn "2:web" "firefox"
+            spawnOn "3:video" "vlc"
+        , borderWidth = 0
+        , terminal = "urxvt"
+        }
